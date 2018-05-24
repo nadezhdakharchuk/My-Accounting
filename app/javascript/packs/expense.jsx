ce5f883618c3c0b172525335d4e94a702aea4511
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2018-05-24 11:45:57 +0300

    fix react form

--- app/javascript/packs/expense.jsx
+++ app/javascript/packs/expense.jsx
@@ -4,41 +4,56 @@ import ReactDOM from "react-dom"
 class ExpenseForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      date: "12/12/18",
+      amount: "500",
+      comment: "lunch"
+    };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleAmountChange = this.handleAmountChange.bind(this);
+    this.handleCommentChange = this.handleCommentChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  handleDateChange(event) {
+    this.setState({ date: event.target.value });
+  }
+
+  handleAmountChange(event) {
+    this.setState({ amount: event.target.value });
+  }
+
+  handleCommentChange(event) {
+    this.setState({ comment: event.target.value });
   }
 
   handleSubmit(event) {
-    alert("Date: " + this.props.date + ",\nAmount: " + this.props.amount + "\nComment: " + this.props.comment);
+    alert("Date: " + this.state.date + ",\nAmount: " + this.state.amount + ",\nComment: " + this.state.comment);
   }
 
   render() {
     return (
-      <form className="form-row" onSubmit={this.handleSubmit} action="/expenses" method="POST">
+      <form className="form-row" onSubmit={this.handleSubmit} action="/expenses" method="post">
         <h2 className="form-group">Expense</h2>
         <div className="row">
           <div className="col-md-4 form-group">
             <label className="control-label" htmlFor="date">
               Date:
             </label>
-            <input id="date" type="text" value={this.props.date} className="form-control" onChange={this.handleChange} />
+            <input id="date" type="text" value={this.state.date} className="form-control" onChange={this.handleDateChange} />
           </div>
           <div className="col-md-4 form-group">
             <label className="control-label" htmlFor="amount">
               Amount:
             </label>
-            <input id="amount" type="text" value={this.props.amount} className="form-control" onChange={this.handleChange} />
+            <input id="amount" type="text" value={this.state.amount} className="form-control" onChange={this.handleAmountChange} />
           </div>
           <div className="col-md-4 form-group">
             <label className="control-label" htmlFor="comment">
               Comment:
             </label>
-            <input id="comment" type="text" value={this.props.comment} className="form-control" onChange={this.handleChange} />
+            <input id="comment" type="text" value={this.state.comment} className="form-control" onChange={this.handleCommentChange} />
           </div>
           <div className="col-md-12 form-group">
             <input type="submit" value="Submit" className="btn btn-primary" />
@@ -50,5 +65,5 @@ class ExpenseForm extends React.Component {
 }
 
 document.addEventListener("DOMContentLoaded", e => {
-  ReactDOM.render(<ExpenseForm date="12/03/18" amount="500" comment="lunch" />, document.getElementById("form"))
+  ReactDOM.render(<ExpenseForm />, document.getElementById("form"))
 })
